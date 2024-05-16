aae5a09edb6d1c41138922f3f2e85ed359273aa5
David Ezequiel Escalada <metalcombi17@gmail.com>
2024-05-16 00:32:22 -0300

    respawn notas
    se agrego un respawn de notas para probar la mecanica de movimiento del jugador.
    las notas se eliminan al salir de la pantalla

--- Assets/Scrips/PlayerScrip/PlayerController.cs
+++ Assets/Scrips/PlayerScrip/PlayerController.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Vector2 upperPosition;
+    [SerializeField] Vector2 lowerPosition;
+    [SerializeField] Vector2 centerPosition ;
+    [SerializeField] float speed;
+    private Vector2 targetPosition;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        targetPosition = centerPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PosicionamientoInputs();
     }
 
+    private void FixedUpdate()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(rb.position, targetPosition, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPosition);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Notas"))
@@ -24,8 +35,19 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void Posicionamiento()
+    private void PosicionamientoInputs()
     {
-
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            targetPosition = upperPosition;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            targetPosition = lowerPosition;
+        }
+        else
+        {
+            targetPosition = centerPosition;
+        }
     }
 }
